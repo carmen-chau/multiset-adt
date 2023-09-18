212ff49172e1e92ad2bd7613816987fcf95676f8
JasonBarahan <122300705+JasonBarahan@users.noreply.github.com>
2023-09-18 16:52:07 -0400

    Thanks Carmen

---
+++ src/MultiSet.java
@@ -0,0 +1,13 @@
+abstract class MultiSet {
+    public abstract boolean add(Object item);
+
+    public abstract void remove(Object item);
+
+    public abstract boolean contains(Object item);
+
+    public abstract boolean is_empty();
+
+    public abstract int count(Object item);
+
+    public abstract int size();
+}
