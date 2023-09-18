643d252f89f22ef2d282b84df9f8c1c55476bc15
carmen-chau <80921817+carmen-chau@users.noreply.github.com>
2023-09-18 16:38:53 -0400

    added initial comments

--- src/Tree.java
+++ src/Tree.java
@@ -1,2 +1,13 @@
 public class Tree {
 }
+/*
+    Carmen Chau's understanding of adts.py
+    There is the MultiSet ADT. This ADT is like a "blueprint" that models the development of the followung classes
+        1. TreeMultiSet (this not only uses the MultiSet ADT but also uses another class called Tree)
+        2. ArrayListMultiSet
+        3. LinkedListMultiSet
+
+     Finally, there is a 4th thing to implement:
+        4. A method called profileMultiSet that runs timing experiments for a given MultiSet implementation
+
+ */
